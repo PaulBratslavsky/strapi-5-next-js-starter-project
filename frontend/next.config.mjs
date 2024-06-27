1d68e9a85bad7cbe9fc2eaa6cd7b5d8c95b6b391
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2024-06-27 00:37:47 -0500

    before deploy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "artistic-card-296c81d2a1.media.strapiapp.com",
+      },
+      {
+        protocol: "https",
         hostname: "placehold.co",
       },
     ],
